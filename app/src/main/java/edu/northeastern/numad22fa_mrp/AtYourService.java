15d214ad60f1a1e2fea490d485b47127fef0b2e2
rahulbaddam13 <rahulrdd007@gmail.com>
2022-10-20 00:32:08 -0400

    Added Three Buttons and activities

---
+++ app/src/main/java/edu/northeastern/numad22fa_mrp/AtYourService.java
@@ -0,0 +1,13 @@
+package edu.northeastern.numad22fa_mrp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AtYourService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_at_your_service);
+    }
+}
--- app/src/main/java/edu/northeastern/numad22fa_mrp/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_mrp/MainActivity.java
@@ -2,13 +2,60 @@ package edu.northeastern.numad22fa_mrp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button atYourService;
+    private Button stickItToEm;
+    private Button project;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        atYourService = (Button)findViewById(R.id.AtYourService);
+        stickItToEm = (Button)findViewById(R.id.StickItToEm);
+        project = (Button)findViewById(R.id.Project);
+
+        atYourService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openA7();
+            }
+        });
+        stickItToEm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openA8();
+
+            }
+        });
+        project.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProject();
+            }
+        });
     }
+
+
+    private void openA7() {
+        Intent intent = new Intent(this, AtYourService.class);
+        startActivity(intent);
+    }
+
+    private void openA8() {
+        Intent intent = new Intent(this, StickItToEm.class);
+        startActivity(intent);
+    }
+
+    private void openProject() {
+        Intent intent = new Intent(this, FinalProject.class);
+        startActivity(intent);
+    }
+
 }
