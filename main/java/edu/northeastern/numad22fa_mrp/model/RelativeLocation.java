c91f8a3f6829890578095b41b33bb8ef56a3aa2c
pranathi13 <98113547+pranathi13@users.noreply.github.com>
2022-10-24 19:31:38 -0400

    Update Weather

--- main/java/edu/northeastern/numad22fa_mrp/model/RelativeLocation.java
+++
@@ -1,9 +0,0 @@
-package edu.northeastern.numad22fa_mrp.model;
-
-public class RelativeLocation {
-    private RelativeLocationProperties properties;
-
-    public RelativeLocationProperties getProperties() {
-        return properties;
-    }
-}
