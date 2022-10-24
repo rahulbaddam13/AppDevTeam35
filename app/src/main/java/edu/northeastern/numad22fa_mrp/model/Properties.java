49eba872ada4be8550d34bc4fc38594008c9a82e
pranathi13 <98113547+pranathi13@users.noreply.github.com>
2022-10-24 19:50:46 -0400

    Added weather

---
+++ app/src/main/java/edu/northeastern/numad22fa_mrp/model/Properties.java
@@ -0,0 +1,24 @@
+package edu.northeastern.numad22fa_mrp.model;
+
+public class Properties {
+    private int gridX;
+    private int gridY;
+    private String cwa;
+    private RelativeLocation relativeLocation;
+
+    public int getGridX() {
+        return gridX;
+    }
+
+    public int getGridY() {
+        return gridY;
+    }
+
+    public String getCwa() {
+        return cwa;
+    }
+
+    public RelativeLocation getLocation() {
+        return relativeLocation;
+    }
+}
---
+++ app/src/main/java/edu/northeastern/numad22fa_mrp/model/WeatherHeader.java
@@ -0,0 +1,10 @@
+package edu.northeastern.numad22fa_mrp.model;
+
+public class WeatherHeader {
+
+    private Properties properties;
+
+    public Properties getProperties() {
+        return properties;
+    }
+}
