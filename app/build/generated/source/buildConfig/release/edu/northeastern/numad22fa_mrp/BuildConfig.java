337c2aab6abb569c5ee24746a9e0be8512d42bc3
rahulbaddam13 <rahulrdd007@gmail.com>
2022-10-24 20:53:11 -0400

    Final App Commit

---
+++ app/build/generated/source/buildConfig/release/edu/northeastern/numad22fa_mrp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.northeastern.numad22fa_mrp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "edu.northeastern.numad22fa_mrp";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
