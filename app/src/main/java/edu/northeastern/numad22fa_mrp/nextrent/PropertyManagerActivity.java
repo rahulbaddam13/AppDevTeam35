378959847328dbca1b5671408daa133fcfd81ed0
MadhuriKrishnamurthy <krishnamurthy.m@northeastern.edu>
2022-11-29 21:18:49 -0500

    Adding new property screen

--- app/src/main/java/edu/northeastern/numad22fa_mrp/nextrent/PropertyManagerActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_mrp/nextrent/PropertyManagerActivity.java
@@ -3,6 +3,10 @@ package edu.northeastern.numad22fa_mrp.nextrent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.service.controls.actions.FloatAction;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import edu.northeastern.numad22fa_mrp.R;
 
@@ -12,5 +16,13 @@ public class PropertyManagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property_manager);
+    }
+    /**
+     * Method that is invoked on click of the floating action button which invokes an activity
+     * to enter values.
+     * @param view current view.
+     */
+    public void onFabPopupWindowClick(View view) {
+
     }
 }
